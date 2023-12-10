9b95bd3209ab8281a9cc8374034bdb5fdc0802c5
BadSantaNF <zeronfmss@gmail.com>
2023-12-10 14:33:52 +0300

    сделал третье  задание

---
+++ task003/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите строку : ");
+            String sl = Console.ReadLine();
+            char[] sym = sl.ToCharArray();
+            if (IsPalindrom(sym))
+             Console.WriteLine("Палиндром");
+            else
+        {         
+            Console.WriteLine("Не палиндром");
+        }
+        static bool IsPalindrom(char[] word)
+        {
+            for (int i = 0; i < word.Length / 2; i++)
+                if (word[i] != word[word.Length - 1 - i])
+            return false;
+            return true;
+        }
